f4568185617fbcb92fa21f5efd4dc3aa1d3c7e29
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-12-16 09:57:53 -0500

    Add register, login and Sanctum routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\API\FederationController;
+use App\Http\Controllers\API\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(RegisterController::class)->group(function () {
+    Route::post('register', 'register')->name('register');
+    Route::post('login', 'login')->name('login');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('federations', FederationController::class);
+    Route::get('user', function (Request $request) {
+        return $request->user();
+    })->name('user');
+});
